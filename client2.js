f3a23d082d51a462242ff54a435b7dc24aebeab5
Michael Adebisi <michael.adebisi@gmail.com>
2019-03-06 10:56:48 +0000

    adding files

---
+++ client2.js
@@ -0,0 +1,33 @@
+const tls = require('tls');
+const fs = require('fs');
+
+const options = {
+    ca: fs.readFileSync('ssl/ca-crt.pem'),
+    key: fs.readFileSync('ssl/client2-key.pem'),
+    cert: fs.readFileSync('ssl/client2-crt.pem'),
+    host: 'server1.testserver',
+    port: 5000,
+    rejectUnauthorized:true,
+    requestCert:true
+};
+
+const socket = tls.connect(options, () => {
+    console.log('client connected', 
+        socket.authorized ? 'authorized' : 'unauthorized');
+    process.stdin.pipe(socket);
+    process.stdin.resume();
+});
+
+socket.setEncoding('utf8');
+
+socket.on('data', (data) => {
+    console.log(data);
+});
+
+socket.on('error', (error) => {
+    console.log(error);
+});
+
+socket.on('end', (data) => {
+    console.log('Socket end event');
+});
---
+++ server.js
@@ -0,0 +1,38 @@
+const tls = require('tls');
+const fs = require('fs');
+
+const options = {
+    ca: fs.readFileSync('ssl/ca-crt.pem'),  
+    key: fs.readFileSync('ssl/server-key.pem'), 
+    cert: fs.readFileSync('ssl/server-crt.pem'), 
+    
+    requestCert: true, 
+    rejectUnauthorized: true
+}; 
+let clients = []
+
+const server = tls.createServer(options, (socket) => {
+
+    console.log('server connected', 
+        socket.authorized ? 'authorized' : 'unauthorized');
+    
+    // connection info
+    //console.log("socket: ",  socket);
+    console.log("Cipher: ",  socket.getCipher());
+    console.log("Address: ", socket.address());
+    console.log("Remote address: ", socket.remoteAddress);
+    console.log("Remote port: ", socket.remotePort);
+
+    socket.on('error', (error) => {
+        console.log(error);
+    });
+    
+    socket.write('welcome!\n');
+    socket.setEncoding('utf8');
+    socket.pipe(process.stdout);
+    socket.pipe(socket);
+});
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log('server listen on port ', port);
+});
